59ffee1ac1a52a9578d64b4cf421ded1a5196dbb
Amit shahwal <56446366+amit-shahwal@users.noreply.github.com>
2020-02-01 01:08:56 +0530

    Add files via upload

---
+++ sih2/login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+$id=$_POST['userid'];
+$password=$_POST['pass'];
+
+$con=mysqli_connect("localhost","root");
+if(!$con)
+    
+echo "not\n";
+mysqli_select_db($con,'sih');
+$q="SELECT * from login where username='$id' && password='$password' ";
+$result=mysqli_query($con,$q);
+$num=mysqli_num_rows($result);
+if($num==2)
+{
+    ?> <script> alert('u_p correct');</script>
+    <?php
+    $_SESSION['userid']=$id;
+    ?>
+    <script>
+        window.location="main.php";
+        </script>
+        <?php
+}
+else
+{
+?> <script>
+    alert('wrong user is and password');
+    window.location="index.php";
+    </script>
+    <?php
+}
+?>
+
+
+
+
